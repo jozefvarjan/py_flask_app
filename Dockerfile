3ef433ee16e4fb252228a6c5064f37512f8c0a02
jvarjan <j.varjan@email.cz>
2026-07-07 00:11:43 +0200

    add put, delete

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.14
-EXPOSE 5001
+EXPOSE 5000
 WORKDIR /py_flask_app
 RUN pip install flask
 COPY . .
+RUN pip install -r requirements.txt
 CMD ["flask", "--app", "main", "run", "--host", "0.0.0.0"]
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request, Response
+from flask_smorest import abort
 from db import stores, items
 
 
@@ -13,6 +14,22 @@ class ResponseMsgObj:
 
 
 # --- store(s)
+
+@app.post('/store')
+def create_store(): 
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {"id": store_id, **store_data}
+    stores[store_id] = new_store
+    response = Response()
+    return ResponseMsgObj(
+        {
+        "status": response.status,
+        "status_code": response.status_code,
+        "data": new_store
+        }
+    )
+
 
 @app.get('/stores')
 def get_stores():
@@ -34,23 +51,30 @@ def get_store(store_id):
                 "err_msg": f"store: {stores[store_id]} not found!"
             }
         )
+    
+
+@app.put("/store/<string:store_id>")
+def update_store(store_id):
+    try:
+        stores[store_id] = request.get_json()["name"]
+        return {"store": stores[store_id]}
+    except KeyError: abort(404, message="not found")
 
 
-@app.post('/store')
-def create_store(): 
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    new_store = {"id": store_id, **store_data}
-    stores[store_id] = new_store
-    response = Response()
-    return ResponseMsgObj(
-        {
-        "status": response.status,
-        "status_code": response.status_code,
-        "data": new_store
-        }
-    )
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"msg": f"store: {store_id} deleted."}
+    except KeyError:
+        abort(404, message=f"store: {store_id} not found!")
 
+
+@app.delete("/stores")   
+def delete_stores():
+    for store in list(stores.keys()):
+        del stores[store]
+    return {"msg": "all stores deleted"} 
 
 
 # --- item(s)
@@ -90,4 +114,20 @@ def create_item():
     )
 
 
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    try:
+        items[item_id] = item_data
+        return items[item_id]
+    except KeyError:
+        abort(404, message=f"item: {item_id} not found!")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+    except KeyError:
+        abort(404, message=f"item: {item_id} not found!")
     
